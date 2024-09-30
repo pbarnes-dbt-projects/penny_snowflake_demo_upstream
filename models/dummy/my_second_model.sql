aeeb632b10cf516bf6ee0e3c24befe0e2c7214e5
Penny Barnes <penny.barnes@fishtownanalytics.com>
2024-09-30 15:22:00 +0000

    Remove dummy

--- models/dummy/my_second_model.sql
+++
@@ -1,9 +0,0 @@
-with source as (
-    select * from {{ ref('my_first_model') }}
-)
-
-select
-    *,
-    my_favorite_number * 2 as my_favorite_number_doubled,
-    my_favorite_number / 2 as my_favorite_number_halved
-from source
